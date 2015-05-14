32aac9f34cb4bcc6e1d131cb1ec09eca5df7f888
Shurup228 <yroslav541@gmail.com>
2015-05-14 21:05:23 +0300

    Kappa

--- src/Engine/Coord.java
+++ src/Engine/Coord.java
@@ -13,11 +13,11 @@ import Engine.Gui_Factory;
  *“от самый коордиатный главный класс
  */
 public class Coord {
-		
+	
 		public static void Fill_Screen() {
 			
 			JPanel Panel = new JPanel();
-			Panel.setBackground(Color.cyan);
+			Panel.setBackground(Color.BLACK);
 			Panel.setLayout(new FlowLayout());
 			
 			Gui_Factory.Kappa.add(Panel);
