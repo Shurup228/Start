b6aafbfe48f72b4b6098b8e41f09c0c5b8ee54a6
Shurup228 <yroslav541@gmail.com>
2015-05-14 20:38:11 +0300

    Kappa

--- src/Engine/Coord.java
+++ src/Engine/Coord.java
@@ -3,12 +3,30 @@
  */
 package Engine;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Window;
+
+import javax.swing.JPanel;
+
+import Engine.Gui_Factory;
+
 /**
  * @author Ярик
  *Тот самый коордиатный главный класс
  */
 public class Coord {
 
+		public static void DrawLine(int x, int y, ) {
+			
+			JPanel Panel = new JPanel();
+			Panel.setBackground(Color.cyan);
+			Panel.setLayout(new FlowLayout());
+			Window.add(Panel);
 	
+	}
 	
 }
