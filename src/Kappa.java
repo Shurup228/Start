3a321fbd52e1e77054a8e9df16a5a296aa459da9
Shurup228 <yroslav541@gmail.com>
2015-05-18 20:26:31 +0300

    Vce eshe ne poyavlyayetsya srazu, inogda

--- src/Kappa.java
+++ src/Kappa.java
@@ -20,6 +20,13 @@ public class Kappa {
 				  Gui_Factory.createWindow();
 				  JButton button1 = Button.CreateButtonSized(20, 10, 5, 3, "Lol", true);
 				  JButton button2 = Button.CreateButtonSized(2 , 1, 2, 2, "Kappa", true);
+				  
+				  for(int i = 0; i == 2; i++) {
+					  
+					  button1.repaint();
+					  button2.repaint();
+					  
+				  }
 
 	}
 
--- src/Util/Game_Tick.java
+++ src/Util/Game_Tick.java
@@ -2,21 +2,20 @@ package Util;
 
 import java.util.Timer;
 import java.util.TimerTask;
-import AI.Artificial_Intelegiance;
-import Engine.Button;
 
 public class Game_Tick extends TimerTask{
 	
-	public static void gameTick()
-	{
+	public static void gameTick() {
+
 		Game_Tick task = new Game_Tick();
 		Timer timer = new Timer();
-        timer.scheduleAtFixedRate(task, 0, 1000);      
+        timer.scheduleAtFixedRate(task, 0, 1000);    
+        
 	}
 
 	public void run() {
+		
 		System.out.println("Ticking");
-		Artificial_Intelegiance.aggroCheck();
 		
 	}
 
